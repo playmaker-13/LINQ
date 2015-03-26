9985c24f9a9d92cc9bee3cda3e7be7db31fe11cb
playmaker-13 <playmaker-13@yandex.ru>
2015-03-26 13:07:14 +0200

    методы расширения

--- Console_LINQ_Example/Console_LINQ_Example/Program.cs
+++ Console_LINQ_Example/Console_LINQ_Example/Program.cs
@@ -10,6 +10,20 @@ namespace Console_LINQ_Example
     {
         static void Main(string[] args)
         {
+            string[] teams = { "Бавария", "Боруссия", "Реал Мадрид", "Манчестер Сити", "ПСЖ", "Барселона" };
+            string[] towns = { "Мельбурн", "Оттава", "Боруссия", "Мадрид", "Барселона"};
+
+            var selectedTowns = towns.Where(q => q.ToUpper().StartsWith("М")).OrderBy(q => q);
+            var selectedTeams = teams.Where(t => t.ToUpper().StartsWith("Б")).OrderBy(t => t);
+            int num = (from i in towns where i.ToUpper().StartsWith("М") select i).Count();
+            int number = (from t in teams where t.ToUpper().StartsWith("Б") select t).Count();
+            foreach (var s in selectedTowns)
+                Console.WriteLine(s);
+            Console.WriteLine(num);
+            foreach (string s in selectedTeams)
+                Console.WriteLine(s);
+            Console.WriteLine(number);
+            Console.ReadLine();
         }
     }
 }
